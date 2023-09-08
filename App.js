afb2667ed93a4cd6e82750d1f97b3e97388e54c7
Paulo Gomes <gomes7.paulo@gmail.com>
2023-09-08 12:04:49 -0300

    Bottom Navigation

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import {createDrawerNavigator, DrawerContentScrollView, DrawerItem} from "@react-navigation/drawer";
-import {SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {Keyboard, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import Permission from "./src/screens/Permisison";
 import Product from "./src/screens/Product";
 import WelcomeScreen from "./src/screens/WelcomeScreen";
@@ -29,15 +29,34 @@ const DrawerContent = (props) => {
 
 const BottomTabs = () => {
     const navigation = useNavigation();
+    const [keyboardStatus, setKeyboardStatus] = useState(undefined);
+
+    useEffect(() => {
+        const showSubscription = Keyboard.addListener('keyboardDidShow', () => {
+            setKeyboardStatus('Keyboard Shown');
+        });
+        const hideSubscription = Keyboard.addListener('keyboardDidHide', () => {
+            setKeyboardStatus('Keyboard Hidden');
+        });
+
+        return () => {
+            showSubscription.remove();
+            hideSubscription.remove();
+        };
+    }, []);
+
+    if (keyboardStatus === 'Keyboard Shown') {
+        return null;
+    }
 
     return (
         <View style={styles.bottomTabs}>
             <TouchableOpacity style={styles.tab} onPress={() => navigation.navigate('Home')}>
-                <Icon name="home" size={24} color="black" />
+                <Icon name="home" size={24} color="gray" />
                 <Text style={styles.tabText}>Home</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.tab} onPress={() => navigation.navigate('Welcome')}>
-                <Icon name="user" size={24} color="black" />
+                <Icon name="user" size={24} color="gray" />
                 <Text style={styles.tabText}>Welcome</Text>
             </TouchableOpacity>
         </View>
@@ -46,6 +65,7 @@ const BottomTabs = () => {
 
 
 export default function App() {
+
     return (
         <SafeAreaView style={{flex: 1}}>
             <NavigationContainer>
@@ -56,8 +76,8 @@ export default function App() {
                         <Drawer.Screen name="Home" component={WelcomeScreen}/>
                         <Drawer.Screen name="Welcome" component={Welcome2}/>
                     </Drawer.Navigator>
-                    <BottomTabs/>
                 </View>
+                <BottomTabs/>
             </NavigationContainer>
         </SafeAreaView>
     );
@@ -67,21 +87,23 @@ const styles = StyleSheet.create({
     bottomTabs: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        backgroundColor: '#b7b5b5',
-        padding: 10,
+        padding: 0,
         position: 'absolute',
         bottom: 0,
         left: 0,
         right: 0,
+        borderTopWidth: 0.5,
+        borderTopColor: '#cdc7c7',
         height: 60,
     },
     tab: {
         flex: 1,
         alignItems: 'center',
         padding: 10,
+
     },
     tabText: {
-        color: 'white'
+        color: 'gray'
     },
 });
 
